53a3e24a1cc83d8e370da6171a73a89251725568
c-mellueh <christoph@mellueh.de>
2024-11-18 09:03:31 +0100

    improve SettingsWidget

--- db_streckenkm/DB_Streckenkm.py
+++ db_streckenkm/DB_Streckenkm.py
@@ -22,52 +22,89 @@
  ***************************************************************************/
 """
 import os.path
-from qgis.core import (
-    QgsProject,
-    QgsPointXY,
-    QgsDistanceArea,
-    QgsSpatialIndex,
-)
-from qgis.gui import QgsMapToolEmitPoint
-from qgis.PyQt.QtCore import QCoreApplication, QSettings, QTranslator
-from qgis.PyQt.QtGui import QIcon
-from qgis.PyQt.QtWidgets import QAction,QMessageBox
-from qgis.core import QgsProject,QgsVectorLayer,QgsGeometry,QgsFeature
-from qgis.gui import QgsMapToolEmitPoint
+import re
 
-# Initialize Qt resources from file resources.py
-# Import the code for the dialog
-from .DB_Streckenkm_dialog import StreckenkmFinderDialog
+from qgis.PyQt.QtCore import QCoreApplication, QSettings, QTranslator, Qt
+from qgis.PyQt.QtGui import QColor, QIcon
+from qgis.PyQt.QtWidgets import QAction, QMessageBox
+from qgis.core import QgsDistanceArea, QgsPointXY, QgsSpatialIndex
+from qgis.core import QgsFeature, QgsGeometry, QgsProject, QgsSimpleLineSymbolLayer, QgsVectorLayer
+from qgis.gui import QgsHighlight, QgsMapToolEmitPoint
+from .ui.settingswidget import SettingsWidget
+
+NEIGHBOR_SAMPLE_SIZE = 100
+
+
+def string_to_real(text: str) -> float | None:
+    if text is None:
+        return None
+    head, tail = re.findall("(-?\d+,\d) \+ (-?\d+)", text)[0]
+    head = float(head.replace(",", "."))
+    return round(head + int(tail) / 1000, 4)
+
 
 class NearestPointFinder(QgsMapToolEmitPoint):
-    def __init__(self,iface,spatial_index,layer,distance_calc):
-        canvas = iface.mapCanvas()
-        super().__init__(canvas)
+    def __init__(self, iface, spatial_index, layer: QgsVectorLayer, distance_calc):
+        self.canvas = iface.mapCanvas()
+        super().__init__(self.canvas)
         self.iface = iface
-        self.spatial_index = spatial_index
+        self.spatial_index: QgsSpatialIndex = spatial_index
         self.layer = layer
         self.distance_calc = distance_calc
-        self.temp_layer:QgsVectorLayer|None = None
+        self.temp_layer: QgsVectorLayer | None = None
         self.create_hidden_layer()
         self.temp_layer_provider = self.temp_layer.dataProvider()
-        self.line:QgsFeature|None = None
-
+        self.line: QgsFeature | None = None
+        self.highlight = None
 
     def create_hidden_layer(self):
-        self.temp_layer =QgsVectorLayer("LineString?crs=EPSG:5683", "Nearest Line", "memory")
+        self.temp_layer = QgsVectorLayer("LineString?crs=EPSG:5683", "Nearest Line", "memory")
         QgsProject.instance().addMapLayer(self.temp_layer)
         tree_view = self.iface.layerTreeView()
         model = self.iface.layerTreeView().layerTreeModel()
         root = QgsProject().instance().layerTreeRoot()
         node = root.findLayer(self.temp_layer.id())
         index = model.node2index(node)
-        tree_view.setRowHidden(index.row(), index.parent(), True )
+        tree_view.setRowHidden(index.row(), index.parent(), True)
         tree_view.setCurrentIndex(model.node2index(root))
+
+        line_symbol = QgsSimpleLineSymbolLayer()
+
+        # Set the line width (thick line)
+        line_symbol.setWidth(2.0)  # Adjust for your desired thickness
+
+        # Set the line color (red)
+        line_symbol.setColor(QColor(255, 0, 0))
+
+        # Set the line style (dotted)
+        line_symbol.setPenStyle(Qt.DotLine)
+
+        # Apply the symbol layer to the layer renderer
+        renderer = self.temp_layer.renderer()
+        symbol = renderer.symbol()
+        symbol.changeSymbolLayer(0, line_symbol)
 
     def __del__(self):
         """Remove the layer from the project."""
         QgsProject.instance().removeMapLayer(self.temp_layer)
         print(f"Layer {self.temp_layer.id()} deleted.")
+        if self.highlight:
+            self.highlight.hide()
+
+    def get_neighbor(self, point: QgsPointXY) -> QgsFeature | None:
+        nearest_ids = self.spatial_index.nearestNeighbor(QgsPointXY(point), NEIGHBOR_SAMPLE_SIZE)
+        nearest_feature = None
+        nearest_dist = float('inf')
+        if not nearest_ids:
+            return None
+        for fid in nearest_ids:
+            feature = self.layer.getFeature(fid)
+            geom = feature.geometry()
+            distance = geom.distance(QgsGeometry.fromPointXY(point))
+            if distance < nearest_dist:
+                nearest_feature = feature
+                nearest_dist = distance
+        return nearest_feature
 
     def canvasReleaseEvent(self, event):
         if not self.layer or not self.spatial_index:
@@ -75,29 +112,83 @@ class NearestPointFinder(QgsMapToolEmitPoint):
             return
 
         # Get the clicked point in map coordinates
-        click_point = self.toMapCoordinates(event.pos())
+        click_point = QgsPointXY(self.toMapCoordinates(event.pos()))
 
         # Find nearest point ID
-        nearest_id = self.spatial_index.nearestNeighbor(QgsPointXY(click_point), 1)
-        if not nearest_id:
+        nearest_feature = self.get_neighbor(click_point)
+        if not nearest_feature:
             QMessageBox.information(None, "Info", "No points found nearby.")
             return
 
-        # Get the nearest feature and its attributes
-        nearest_feature = self.layer.getFeature(nearest_id[0])
+        self.highlight_feature(nearest_feature)
         nearest_geom = nearest_feature.geometry()
-        distance = self.distance_calc.measureLine(QgsPointXY(click_point), nearest_geom.asPoint())
-        strecken_nr = nearest_feature["GKA_STRECKEGLEISNR"] if "GKA_STRECKEGLEISNR" in nearest_feature.fields().names() else "No StreckengleisNr"
-        strecken_km = nearest_feature["strecken_km"] if "strecken_km" in nearest_feature.fields().names() else "No StreckenKm"
-        self.draw_line(QgsPointXY(click_point), nearest_geom.asPoint())
+        rec_dist, closest_point, next_index, is_left = nearest_geom.closestSegmentWithContext(click_point)
+        dist = self.get_partial_line_length(nearest_feature.geometry(), next_index - 1, closest_point)
+        strecken_nr = nearest_feature[
+            "GKA_STRECKEGLEISNR"] if "GKA_STRECKEGLEISNR" in nearest_feature.fields().names() else "No StreckengleisNr"
+        strecken_km_text = nearest_feature[
+            "VON_KM_V"] if "VON_KM_V" in nearest_feature.fields().names() else "No StreckenKm"
+
+        self.draw_line(QgsPointXY(click_point), closest_point)
+        if not strecken_km_text:
+            QMessageBox.information(None, "Kein Hauptgleis", "Streckenkilometer nicht vorhanden")
+            return
+        position = string_to_real(strecken_km_text) + dist / 1000
 
         # Display information
         QMessageBox.information(None, "StreckenNr",
-                                f"Name: {strecken_nr}\nStrecken Km: {strecken_km}")
+                                f"Name: {strecken_nr}\nStrecken Km: {position:.3f}\n index: {next_index}")
+
+    def get_partial_line_length(self, line: QgsGeometry, index: int, new_point):
+        line_strings = line.asMultiPolyline()
+        cumulative_length = 0
+        current_segment = 0
+        last_point = [line_strings[0][0]]
+        for line in line_strings:
+            vertices = line  # Vertices of the current LineString
+            last_point = vertices[0]
+            num_segments = len(vertices) - 1  # Number of segments in the current LineString
+
+            # Check if the target segment index is within this LineString
+            if current_segment + num_segments > index:
+                # Calculate the partial length in this LineString
+                target_index = index - current_segment  # Segment index within this LineString
+                last_point = vertices[target_index]
+                partial_vertices = vertices[:target_index + 1]  # Include points up to the target segment
+                partial_line = QgsGeometry.fromPolylineXY(partial_vertices)
+                cumulative_length += partial_line.length()
+                break
+            else:
+                # Add the full length of this LineString
+                full_line = QgsGeometry.fromPolylineXY(vertices)
+                cumulative_length += full_line.length()
+                current_segment += num_segments
+        partial_line = QgsGeometry.fromPolylineXY([last_point, new_point])
+        cumulative_length += partial_line.length()
+        return cumulative_length
+
+    def highlight_feature(self, feature: QgsFeature):
+        # Remove previous highlight
+        self.hide_highlight()
+
+        # Highlight the new feature
+        self.highlight = QgsHighlight(self.canvas, feature, self.layer)
+        self.highlight.setColor(QColor(255, 0, 0))  # Red highlight
+        self.highlight.setWidth(2)
+        self.highlight.show()
+
+    def hide_highlight(self):
+        if self.highlight:
+            self.highlight.hide()
+
+    def delete_lines(self):
+        self.temp_layer_provider.truncate()
+        self.temp_layer.updateExtents()
+        self.temp_layer.triggerRepaint()
 
     def draw_line(self, start_point, end_point):
         # Create a line feature
-        self.temp_layer_provider.truncate()
+        self.delete_lines()
 
         line_geom = QgsGeometry.fromPolylineXY([start_point, end_point])
         line_feature = QgsFeature()
@@ -107,6 +198,7 @@ class NearestPointFinder(QgsMapToolEmitPoint):
         self.temp_layer.updateExtents()
         self.temp_layer.triggerRepaint()
         self.line = line_feature
+
 
 class StreckenkmFinder:
     """QGIS Plugin Implementation."""
@@ -135,6 +227,7 @@ class StreckenkmFinder:
             self.translator.load(locale_path)
             QCoreApplication.installTranslator(self.translator)
 
+
         # Declare instance attributes
         self.actions = []
         self.menu = self.tr(u'&DB_Streckenkm')
@@ -143,16 +236,29 @@ class StreckenkmFinder:
         # Must be set in initGui() to survive plugin reloads
         self.first_start = None
         self.canvas = iface.mapCanvas()
-        self.layer = None
         self.spatial_index = None
         self.distance_calc = QgsDistanceArea()
         self.distance_calc.setEllipsoid('WGS84')
-        self.map_tool = None
+        self.iface.mapCanvas().mapToolSet.connect(self.map_tool_changed)
+
+
+        # Create Settings Widget
+        self.settings_widget = SettingsWidget()
+        self.layer = None
+        self.field_name = None
+        self.field_is_real = None
+        self.ignore_sidings = None
+        self.connect_settings_widget()
+
+        # Declare MapTool
+        self.map_tool: NearestPointFinder|None = None
+
     def create_spatial_index(self):
         # Build spatial index for the point layer
         self.spatial_index = QgsSpatialIndex(self.layer.getFeatures())
 
         # noinspection PyMethodMayBeStatic
+
     def tr(self, message):
         """Get the translation for a string using Qt translation API.
 
@@ -244,12 +350,13 @@ class StreckenkmFinder:
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
-        icon_path = ':/plugins/DB_Streckenkm/icon.png'
+        icon_path = os.path.join(os.path.dirname(__file__), "icon.png")
         self.add_action(
             icon_path,
             text=self.tr(u'Streckenkilometer'),
             callback=self.run,
-            parent=self.iface.mainWindow())
+            parent=self.iface.mainWindow(),
+            add_to_toolbar=True)
 
         # will be set False in run()
         self.first_start = True
@@ -262,25 +369,26 @@ class StreckenkmFinder:
                 action)
             self.iface.removeToolBarIcon(action)
 
+    def connect_settings_widget(self):
+        self.settings_widget.accept.connect(self.update_settings)
 
-    def select_layer(self):
-        self.dlg = StreckenkmFinderDialog()
-        layers = QgsProject.instance().layerTreeRoot().children()
-        # Clear the contents of the comboBox from previous runs
-        self.dlg.comboBox.clear()
-        # Populate the comboBox with names of all the loaded layers
-        self.dlg.comboBox.addItems([layer.name() for layer in layers])
-
-        # show the dialog
-        self.dlg.show()
-        # Run the dialog event loop
-        result = self.dlg.exec_()
-        if result:
-            self.layer = layers[self.dlg.comboBox.currentIndex()].layer()
+    def update_settings(self):
+        self.layer,self.field_name,self.field_is_real,self.ignore_sidings = self.settings_widget.get_selected_settings()
+        if self.layer:
             self.create_spatial_index()
 
+    def open_settings(self):
+        self.settings_widget.reload_layer_combobox(self.layer)
+        self.settings_widget.reload_attribute_combobox(self.field_name)
+        self.settings_widget.show()
+
+    def map_tool_changed(self):
+        if self.map_tool is not None:
+            self.map_tool.hide_highlight()
+            self.map_tool.delete_lines()
+
     def set_map_tool(self):
-        self.map_tool = NearestPointFinder(self.iface,self.spatial_index,self.layer,self.distance_calc)
+        self.map_tool = NearestPointFinder(self.iface, self.spatial_index, self.layer, self.distance_calc)
         self.iface.mapCanvas().setMapTool(self.map_tool)
 
     def run(self):
@@ -289,7 +397,7 @@ class StreckenkmFinder:
         # Create the dialog with elements (after translation) and keep reference
         # Only create GUI ONCE in callback, so that it will only load when the plugin is started
         if self.spatial_index is None:
-            self.select_layer()
+            self.open_settings()
         else:
             self.set_map_tool()
             # Fetch the currently loaded layers
